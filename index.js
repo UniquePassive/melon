302afb0dddd3cc31b482b9bc6106b5a9d93b56c7
UniquePassive <prozet@live.se>
2017-09-08 21:30:09 +0200

    Add message editing logs

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const LOGIN_TOKEN = '';
 const LOG_CHANNEL = 'melon';
 
 const MESSAGE_DELETED_EVENT_ID = '#0';
-const MESSAGE_DELETED_MESSAGE = "**Meddelande bortsopat från {channel} **";
+const MESSAGE_DELETED_MESSAGE = "**Meddelande bortsopat från {channel}**";
 const MESSAGE_DELETED_COLOR = 'FF470F';
+
+const MESSAGE_UPDATED_EVENT_ID = '#1';
+const MESSAGE_UPDATED_MESSAGE = "**Meddelande i {channel} redigerat**\n\"{oldmsg}\"\n**till**\n\"{newmsg}\"";
+const MESSAGE_UPDATED_COLOR = 'FF470F';
 
 require('format-unicorn') // this adds formatUnicorn to String.prototype
 const Discord = require('discord.js');
@@ -42,6 +46,24 @@ client.on('message', message => {
   }
 });
 
+client.on('messageUpdate', (oldmsg, newmsg) => {
+  if (oldmsg.author === client.user) {
+    return;
+  }
+
+  // Post a log in LOG_CHANNEL upon message update
+  const channel = oldmsg.guild.channels
+    .find(channel => channel.type === 'text' && channel.name === LOG_CHANNEL);
+
+  channel.send(new Discord.RichEmbed({
+    author: { name: oldmsg.author.tag },
+    color: Number('0x' + MESSAGE_UPDATED_COLOR),
+    description: MESSAGE_UPDATED_MESSAGE.formatUnicorn({ channel: oldmsg.channel, oldmsg: oldmsg.content, newmsg: newmsg.content }),
+    thumbnail: { url: oldmsg.author.avatarURL },
+    footer: { text: MESSAGE_UPDATED_EVENT_ID }
+  }));
+});
+
 client.on('messageDelete', message => {
   // Post a log in LOG_CHANNEL upon message deletion
   const channel = message.guild.channels
@@ -51,7 +73,8 @@ client.on('messageDelete', message => {
     // Repost message deletion log if it was deleted
     const embed = message.embeds[0];
 
-    if (embed.footer.text === MESSAGE_DELETED_EVENT_ID) {
+    if (embed.footer.text === MESSAGE_DELETED_EVENT_ID 
+        || embed.footer.text === MESSAGE_UPDATED_EVENT_ID) {
       channel.send(new Discord.RichEmbed({
         author: { name: embed.author.name },
         color: embed.color,
